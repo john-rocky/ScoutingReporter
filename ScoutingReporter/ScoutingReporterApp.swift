984503d050f7ced49ac8137b0fb70801625a7653
john-rocky <john-rocky@hubot.com>
2021-11-10 10:37:03 +0900

    Initial Commit

---
+++ ScoutingReporter/ScoutingReporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoutingReporterApp.swift
+//  ScoutingReporter
+//
+//  Created by 間嶋大輔 on 2021/11/10.
+//
+
+import SwiftUI
+
+@main
+struct ScoutingReporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
